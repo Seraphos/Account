3f97bd8d141ac75d47ff9fe35d94c4d6bf8c7d6c
Andrew Connelly <andrew.connelly@spgusa.com>
2018-06-27 14:40:51 -0400

    Add project files.

---
+++ Account/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Account
+{
+    class Account
+    {
+        private decimal balance; //instance variable, stores balance
+
+        //contructor
+        public Account(decimal initialBalance) { Balance = initialBalance; } //end DecimalType Constructor
+
+        //credit (add) an amount to the account
+        public void Credit(decimal amount) { Balance = Balance + amount; } //end Credit method
+
+        //a property to get & set the account balance
+        public decimal Balance
+        {
+            get { return balance; } //end get
+            set
+            {   //validate that the value is greater than or equal to 0;
+                //if not, the balance is unchanged.
+                if (value >= 0) balance = value;
+            }//end set
+        }//end Balance method
+
+        public void Debit(decimal amount)
+        {
+            if (amount >= balance)
+            {
+                amount = 0;
+                Console.WriteLine("Debit amount exceeded balance. \nNo change made to the balance on this account.");
+            }
+            balance = balance - amount;
+        }//end debit method
+
+    }
+}
---
+++ Account/AccountTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account
+{
+    class AccountTest
+    {
+        public static void Main(string[] args)
+        {
+            Account account1 = new Account(50.00M);
+            Account account2 = new Account(-7.43M);
+
+            Console.WriteLine("Account 1 Balance: {0:C}", account1.Balance);
+            Console.WriteLine("Account 2 Balance: {0:C}", account2.Balance);
+
+            decimal depositAmount;
+
+            Console.WriteLine("Enter deposit amount for account 1: ");
+            depositAmount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Adding {0:C} to account 1 balance\n", depositAmount);
+
+            account1.Credit(depositAmount); //add to account1 balance
+
+            Console.WriteLine("account 1 balance: {0:C}", account1.Balance);
+            Console.WriteLine("account 2 balance: {0:C}", account2.Balance);
+
+            Console.WriteLine("Enter deposit amount for account 2: ");
+            depositAmount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("adding {0:C} to account 2 balance\n", depositAmount);
+            account2.Credit(depositAmount);
+            Console.WriteLine("Account 1 balance: {0:C}", account1.Balance);
+            Console.WriteLine("Account 2 balance: {0:C}", account2.Balance);
+
+            decimal debitAmount1, debitAmount2;
+
+            Console.WriteLine("Enter a debit for account 1: ");
+            debitAmount1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter a debit for account2: ");
+            debitAmount2 = Convert.ToDecimal(Console.ReadLine());
+            account1.Debit(debitAmount1);
+            account2.Debit(debitAmount2);
+
+            Console.WriteLine("Account 1 value after debits: {0:C}\nAccount 2 value after debits: {1:C}", account1.Balance, account2.Balance);
+            Console.ReadKey();
+        }//end main method
+    }
+}
